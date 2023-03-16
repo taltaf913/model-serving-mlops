15d2862ed23a2d6a374394b25800703d38f38074
Niall Turbitt <niall.turbitt@databricks.com>
2023-03-16 15:53:09 +0000

    remove unused sh command

--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -5,9 +5,7 @@ set +e  # temporarily disables the "exit immediately on error" option (-e) in Ba
 FAILURE=false
 
 # pytest configuration set in pyproject.toml
-python -m pytest || FAILURE=true
-
-./training/tests/test_run_experiment.sh || FAILURE=true  # || operator means "or", so if pytest fails (i.e., returns a non-zero status code), then the FAILURE variable is set to true.
+python -m pytest || FAILURE=true # || operator means "or", so if pytest fails (i.e., returns a non-zero status code), then the FAILURE variable is set to true.
 
 if [ "$FAILURE" = true ]; then
   echo "Unit tests failed"
